1cd8b048ea10d8a570feace30098fb42e382fa0e
kalam ahmed <kalamahmed714@gmail.com>
2020-12-02 11:11:44 +0600

    setup g-captcha and succsefully finished the project

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,8 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required','min:4', 'confirmed'],
-            'phone'=>['required']
+            'phone'=>['required'],
+            
         ]);
     }
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -87,6 +87,13 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                
+                                
+                                <div class="g-recaptcha"  data-sitekey="6LeDyvUZAAAAAJRLEB5Z-ZUx5rjlQoa_EAh7k-LF"></div>
+                            </div>
+                        </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
